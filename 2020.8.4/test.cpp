7ce5cacac65418a3f972d7b0fe4f9edffc80c870
Centos_chen <1109555165@qq.com>
2020-08-04 00:00:14 +0000

    2020.8.4

--- 2020.8.4/test.cpp
+++ 2020.8.4/test.cpp
@@ -1,30 +1,37 @@
-//给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
-//最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
-//你可以假设除了整数 0 之外，这个整数不会以零开头。
+//给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
 #include <iostream>
+#include <string>
 #include <vector>
 using namespace std;
 
 class Solution {
     public:
-        vector<int> plusOne(vector<int>& digits) 
+        void moveZeroes(vector<int>& nums) 
         {
-            int len=digits.size();
-            for(int i=len-1;i>=0;i--)
+            if (nums.size() == 0)
+                return;
+            int p1 = 0, p2 = 0;
+            while (p1 < nums.size())
             {
-                if(digits[i]==9) 
-                    digits[i]=0;
-                else
-                { 
-                    digits[i]++;
-                    return digits;
+                if (nums[p1] == 0)
+                {
+                    for (; p2 < nums.size();p2++)
+                    {
+                        if (nums[p2] != 0)
+                            break;
+
+                    }
+                    if (p2 == nums.size())
+                        return;
+                    std::swap(nums[p1], nums[p2]);
 
                 }
+                p1++;
+                if (p1>p2)
+                    p2 = p1 + 1;
 
             }
-            digits.push_back(0);
-            digits[0]=1;
-            return digits;
 
         }
 
